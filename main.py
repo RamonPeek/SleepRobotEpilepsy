381b3f164a1aa822700d37ba4428f2d34967ea6e
RamonPeek <ramonpeek@hotmail.com>
2021-09-01 12:06:56 +0200

    Added continuously recording and printing input volume/pitch

--- main.py
+++ main.py
@@ -1,8 +1,70 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# This is a simple demonstration on how to stream
+# audio from microphone and then extract the pitch
+# and volume directly with help of PyAudio and Aubio
+# Python libraries. The PyAudio is used to interface
+# the computer microphone. While the Aubio is used as
+# a pitch detection object. There is also NumPy
+# as well to convert format between PyAudio into
+# the Aubio.
+import aubio
+import numpy as num
+import pyaudio
+import sys
 
+# Some constants for setting the PyAudio and the
+# Aubio.
+BUFFER_SIZE             = 2048
+CHANNELS                = 1
+FORMAT                  = pyaudio.paFloat32
+METHOD                  = "default"
+SAMPLE_RATE             = 44100
+HOP_SIZE                = BUFFER_SIZE//2
+PERIOD_SIZE_IN_FRAME    = HOP_SIZE
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main(args):
 
+    # Initiating PyAudio object.
+    pA = pyaudio.PyAudio()
+    info = pA.get_host_api_info_by_index(0)
+    numdevices = info.get('deviceCount')
+    for i in range(0, numdevices):
+        if (pA.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+            print("Input Device id ", i, " - ", pA.get_device_info_by_host_api_device_index(0, i).get('name'))
+
+    # Open the microphone stream.
+    mic = pA.open(format=FORMAT, channels=CHANNELS,
+        rate=SAMPLE_RATE, input=True,
+        frames_per_buffer=PERIOD_SIZE_IN_FRAME,
+        input_device_index=4
+        )
+
+    # Initiating Aubio's pitch detection object.
+    pDetection = aubio.pitch(METHOD, BUFFER_SIZE,
+        HOP_SIZE, SAMPLE_RATE)
+    # Set unit.
+    pDetection.set_unit("Hz")
+    # Frequency under -40 dB will considered
+    # as a silence.
+    pDetection.set_silence(-40)
+
+    # Infinite loop!
+    while True:
+
+        # Always listening to the microphone.
+        data = mic.read(PERIOD_SIZE_IN_FRAME)
+        # Convert into number that Aubio understand.
+        samples = num.fromstring(data,
+            dtype=aubio.float_type)
+        # Finally get the pitch.
+        pitch = pDetection(samples)[0]
+        # Compute the energy (volume)
+        # of the current frame.
+        volume = num.sum(samples**2)/len(samples)
+        # Format the volume output so it only
+        # displays at most six numbers behind 0.
+        volume = "{:6f}".format(volume)
+
+        # Finally print the pitch and the volume.
+        print(str(pitch) + " " + str(volume))
+
+if __name__ == "__main__": main(sys.argv)
